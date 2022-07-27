9c89e57d7c645186b6119efcb98357f9cc09ecc4
Francois Polo <andasan@gmail.com>
2022-07-27 15:47:03 -0700

    :sparkles: add some light to the model

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import Head from 'next/head'
-import { Suspense } from "react"
-import { Canvas, useLoader } from "@react-three/fiber"
-import { Environment, OrbitControls } from "@react-three/drei"
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+import Head from "next/head";
+import { Suspense } from "react";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const Model = () => {
   // location of the 3D model
@@ -16,19 +16,24 @@ const Model = () => {
 };
 
 function App() {
-
   return (
     <div className="globe">
       <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
+        <ambientLight intensity={0.7} />
+        <spotLight
+          intensity={0.5}
+          angle={0.1}
+          penumbra={1}
+          position={[10, 15, 10]}
+          castShadow
+        />
         <Suspense fallback={null}>
           <Model />
-          {/* To add environment effect to the model */}
           <Environment preset="city" />
         </Suspense>
       </Canvas>
     </div>
   );
-
 }
 
 export default App;
